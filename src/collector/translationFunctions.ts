6aecfa5f5cafa1f479e20f214c6cba9cee5d6ab5
Ruben Gees <rubengees7@gmail.com>
2026-04-16 22:11:06 +0200

    Configure project for node 22 and fix simplify regex handling

--- src/collector/translationFunctions.ts
+++ src/collector/translationFunctions.ts
@@ -1,12 +1,4 @@
 export const TRANSLATION_FUNCTIONS = new Set(["t", "te", "tm", "tc", "$t", "$te", "$tm", "$tc"])
 
 // Regex for checking if text contains a translation function. To be used to skip full parsing.
-export const TRANSLATION_CALL_REGEX = new RegExp(
-  "(?:" +
-    TRANSLATION_FUNCTIONS.values()
-      .toArray()
-      .sort((a, b) => b.length - a.length)
-      .map((fn) => RegExp.escape(fn))
-      .join("|") +
-    ")\\s*\\(",
-)
+export const TRANSLATION_CALL_REGEX = /\$?t[emc]?\s*\(/
