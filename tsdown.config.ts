e040169db33b105b979a252a401c6e99ad1718e5
Ruben Gees <rubengees7@gmail.com>
2026-09-10 00:52:10 +0200

    Fix build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "tsdown"
 export default defineConfig([
   {
     entry: { index: "src/index.ts" },
-    dts: {
-      tsgo: true,
-    },
   },
   {
     entry: { cli: "src/cli.ts" },
