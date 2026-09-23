31f4e636b98fbb815f4e9bf024d637ef181aa86d
Ruben Gees <rubengees7@gmail.com>
2026-09-23 21:33:04 +0200

    Support rt translation function

--- src/collector/translationFunctions.ts
+++ src/collector/translationFunctions.ts
@@ -1,4 +1,4 @@
-export const TRANSLATION_FUNCTIONS = new Set(["t", "te", "tm", "tc", "$t", "$te", "$tm", "$tc"])
+export const TRANSLATION_FUNCTIONS = new Set(["t", "te", "tm", "tc", "$t", "$te", "$tm", "$tc", "rt", "$rt"])
 
 // Regex for checking if text contains a translation function. To be used to skip full parsing.
 export const TRANSLATION_CALL_REGEX = /\$?t[emc]?\s*\(/
